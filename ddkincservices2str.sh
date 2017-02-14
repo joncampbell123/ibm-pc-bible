70742f599fc145357420fd8f8e1bdc4b51bd8a19
Jonathan Campbell <jonathan@castus.tv>
2017-02-13 23:11:40 -0800

    more

--- ddkincservices2str.sh
+++ ddkincservices2str.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 #
-# Example script to take VMCPD.INC and dump all VMCPD_Service enumerations
-#file=/mnt/main/emu/win31ddk/DDK/386/include/vmcpd.inc
-#file=~/Downloads/x/ddk95/Inc32/VMCPD.INC
-#file=~/Downloads/win98ddk/98ddk/inc/win98/VMCPD.INC
-file=~/Downloads/winmeddk/NTDDK/inc/win_me/vmcpd.inc
+# Example script to take EBIOS.INC and dump all EBIOS_Service enumerations
+#file=/mnt/main/emu/win31ddk/DDK/386/include/ebios.inc
+#file=~/Downloads/x/ddk95/Inc32/EBIOS.INC
+#file=~/Downloads/win98ddk/98ddk/inc/win98/EBIOS.INC
+file=~/Downloads/winmeddk/NTDDK/inc/win_me/ebios.inc
 count=0
 
 ver="4.90+"
-device=0x0011
+device=0x0012
 
-grep -R "VMCPD_Service" $file | cut -d $'\t' -f 2 | cut -d ',' -f 1 | sed -e 's/\x0D//g' | sed -e "s/VMCPD_Service *//" | while read X; do
+grep -R "EBIOS_Service" $file | cut -d $'\t' -f 2 | cut -d ',' -f 1 | sed -e 's/\x0D//g' | sed -e "s/EBIOS_Service *//" | while read X; do
     counth=`printf '%-8s %04XH %04XH  %s' "$ver" $device $count "$X"`;
     echo "$counth"
     count=$(($count+1))
