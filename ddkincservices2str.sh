933c4ee9d67d04a9a127653cc8014b4245f7d794
Jonathan Campbell <jonathan@castus.tv>
2017-02-13 23:15:49 -0800

    more

--- ddkincservices2str.sh
+++ ddkincservices2str.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 #
-# Example script to take BIOSXlat.INC and dump all BIOSXlat_Service enumerations
-#file=/mnt/main/emu/win31ddk/DDK/386/include/biosxlat.inc
-#file=~/Downloads/x/ddk95/Inc32/BIOSXLAT.INC
-#file=~/Downloads/win98ddk/98ddk/inc/win98/BIOSXLAT.INC
-#file=~/Downloads/winmeddk/NTDDK/inc/win_me/biosxlat.inc
+# Example script to take VNETBIOS.INC and dump all VNETBIOS_Service enumerations
+#file=/mnt/main/emu/win31ddk/DDK/386/include/vnetbios.inc
+#file=~/Downloads/x/ddk95/Inc32/VNETBIOS.INC
+#file=~/Downloads/win98ddk/98ddk/inc/win98/VNETBIOS.INC
+file=~/Downloads/winmeddk/NTDDK/inc/win_me/vnetbios.inc
 count=0
 
-ver="3.1+"
-device=0x0013
+ver="4.90+"
+device=0x0014
 
-grep -R "BIOSXlat_Service" $file | cut -d $'\t' -f 2 | cut -d ',' -f 1 | sed -e 's/\x0D//g' | sed -e "s/BIOSXlat_Service *//" | while read X; do
+grep -R "VNETBIOS_Service" $file | cut -d $'\t' -f 2 | cut -d ',' -f 1 | sed -e 's/\x0D//g' | sed -e "s/VNETBIOS_Service *//" | while read X; do
     counth=`printf '%-8s %04XH %04XH  %s' "$ver" $device $count "$X"`;
     echo "$counth"
     count=$(($count+1))
