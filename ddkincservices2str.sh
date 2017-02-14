d5f09ea8b4404ff398087bd4e78da1545e7b8671
Jonathan Campbell <jonathan@castus.tv>
2017-02-13 22:39:44 -0800

    VSD symbols

--- ddkincservices2str.sh
+++ ddkincservices2str.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 #
-# Example script to take VDD.INC and dump all VDD_Service enumerations
-#file=/mnt/main/emu/win31ddk/DDK/386/include/vdd.inc
-#file=~/Downloads/x/ddk95/Inc32/MINIVDD.INC
-#file=~/Downloads/win98ddk/98ddk/inc/win98/MINIVDD.INC
-file=~/Downloads/winmeddk/NTDDK/inc/win_me/minivdd.inc
+# Example script to take VSD.INC and dump all VSD_Service enumerations
+#file=/mnt/main/emu/win31ddk/DDK/386/include/vsd.inc
+#file=~/Downloads/x/ddk95/Inc32/VSD.INC
+#file=~/Downloads/win98ddk/98ddk/inc/win98/VSD.INC
+file=~/Downloads/winmeddk/NTDDK/inc/win_me/vsd.inc
 count=0
 
 ver="4.90+"
-device=0x000A
+device=0x000B
 
-grep -R "VDD_Service" $file | cut -d $'\t' -f 2 | cut -d ',' -f 1 | sed -e 's/\x0D//g' | sed -e "s/VDD_Service *//" | while read X; do
+grep -R "VSD_Service" $file | cut -d $'\t' -f 2 | cut -d ',' -f 1 | sed -e 's/\x0D//g' | sed -e "s/VSD_Service *//" | while read X; do
     counth=`printf '%-8s %04XH %04XH  %s' "$ver" $device $count "$X"`;
     echo "$counth"
     count=$(($count+1))
