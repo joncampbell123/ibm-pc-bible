05ca70056f579c01e4cddbda0ebe25198a83a8c9
Jonathan Campbell <jonathan@castus.tv>
2017-02-14 15:43:49 -0800

    ENABLE---er, I mean--VMINI

--- ddkincservices2str.sh
+++ ddkincservices2str.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 #
-# Example script to take VFBACKUP.INC and dump all VFBACKUP_Service enumerations
-#file=/mnt/main/emu/win31ddk/DDK/386/include/vfbackup.inc
-#file=~/Downloads/x/ddk95/Inc32/VFBACKUP.INC
-#file=~/Downloads/win98ddk/98ddk/inc/win98/VFBACKUP.INC
-file=~/Downloads/winmeddk/NTDDK/inc/win_me/vfbackup.inc
+# Example script to take ENABLE.INC and dump all ENABLE_Service enumerations
+#file=/mnt/main/emu/win31ddk/DDK/386/include/enable.inc
+#file=~/Downloads/x/ddk95/Inc32/ENABLE.INC
+#file=~/Downloads/win98ddk/98ddk/inc/win98/ENABLE.INC
+file=~/Downloads/winmeddk/NTDDK/inc/win_me/enable.inc
 count=0
 
 ver="4.90+"
-device=0x0036
+device=0x0037
 
-grep -R "VFBACKUP_Service" $file | sed "s/^[\t ]*//" | sed "s/[\t ]\+/"$'\t'"/g" | cut -d $'\t' -f 2 | cut -d ',' -f 1 | sed -e 's/\x0D//g' | sed -e "s/VFBACKUP_Service *//" | while read X; do
+grep -R "ENABLE_Service" $file | sed "s/^[\t ]*//" | sed "s/[\t ]\+/"$'\t'"/g" | cut -d $'\t' -f 2 | cut -d ',' -f 1 | sed -e 's/\x0D//g' | sed -e "s/ENABLE_Service *//" | while read X; do
     counth=`printf '%-8s %04XH %04XH  %s' "$ver" $device $count "$X"`;
     echo "$counth"
     count=$(($count+1))
