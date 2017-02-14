8a24717877c5adc0fd602322701aa5c8092023d7
Jonathan Campbell <jonathan@castus.tv>
2017-02-14 15:50:35 -0800

    IFSMGR

--- ddkincservices2str.sh
+++ ddkincservices2str.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 #
-# Example script to take VCOND.INC and dump all VCOND_Service enumerations
-#file=/mnt/main/emu/win31ddk/DDK/386/include/vcond.inc
-#file=~/Downloads/x/ddk95/Inc32/VCOND.INC
-#file=~/Downloads/win98ddk/98ddk/inc/win98/VCOND.INC
-file=~/Downloads/winmeddk/NTDDK/inc/win_me/vcond.inc
+# Example script to take IFSMGR.INC and dump all IFSMgr_Service enumerations
+#file=/mnt/main/emu/win31ddk/DDK/386/include/ifsmgr.inc
+#file=~/Downloads/x/ddk95/Inc32/IFSMGR.INC
+#file=~/Downloads/win98ddk/98ddk/inc/win98/IFSMGR.INC
+file=~/Downloads/winmeddk/NTDDK/inc/win_me/ifsmgr.inc
 count=0
 
 ver="4.90+"
-device=0x0038
+device=0x0040
 
-grep -R "VCOND_Service" $file | sed "s/^[\t ]*//" | sed "s/[\t ]\+/"$'\t'"/g" | cut -d $'\t' -f 2 | cut -d ',' -f 1 | sed -e 's/\x0D//g' | sed -e "s/VCOND_Service *//" | while read X; do
+grep -R "IFSMgr_Service" $file | sed "s/^[\t ]*//" | sed "s/[\t ]\+/"$'\t'"/g" | cut -d $'\t' -f 2 | cut -d ',' -f 1 | sed -e 's/\x0D//g' | sed -e "s/IFSMgr_Service *//" | while read X; do
     counth=`printf '%-8s %04XH %04XH  %s' "$ver" $device $count "$X"`;
     echo "$counth"
     count=$(($count+1))
