9edf66fa2d72cda6080356c63aa4cfa7b06a081c
Jonathan Campbell <jonathan@castus.tv>
2017-02-13 22:24:44 -0800

    V86MMGR symbols

--- ddkincservices2str.sh
+++ ddkincservices2str.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 #
-# Example script to take VTD.INC and dump all VTD_Service enumerations
-#file=/mnt/main/emu/win31ddk/DDK/386/include/vtd.inc
-#file=~/Downloads/x/ddk95/Inc32/VTD.INC
-#file=~/Downloads/win98ddk/98ddk/inc/win98/VTD.INC
-file=~/Downloads/winmeddk/NTDDK/inc/win_me/vtd.inc
+# Example script to take V86MMGR.INC and dump all V86MMGR_Service enumerations
+#file=/mnt/main/emu/win31ddk/DDK/386/include/v86mmgr.inc
+#file=~/Downloads/x/ddk95/Inc32/V86MMGR.INC
+#file=~/Downloads/win98ddk/98ddk/inc/win98/V86MMGR.INC
+file=~/Downloads/winmeddk/NTDDK/inc/win_me/v86mmgr.inc
 count=0
 
 ver="4.90+"
-device=0x0005
+device=0x0006
 
-grep -R "VTD_Service" $file | cut -d $'\t' -f 2 | cut -d ',' -f 1 | sed -e 's/\x0D//g' | sed -e "s/VTD_Service *//" | while read X; do
+grep -R "V86MMGR_Service" $file | cut -d $'\t' -f 2 | cut -d ',' -f 1 | sed -e 's/\x0D//g' | sed -e "s/V86MMGR_Service *//" | while read X; do
     counth=`printf '%-8s %04XH %04XH  %s' "$ver" $device $count "$X"`;
     echo "$counth"
     count=$(($count+1))
