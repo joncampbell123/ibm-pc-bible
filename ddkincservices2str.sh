a70a01d7b8ed1e9f428ed068c9c1e94557cf6d27
Jonathan Campbell <jonathan@castus.tv>
2017-02-13 22:27:27 -0800

    Pageswap symbols

--- ddkincservices2str.sh
+++ ddkincservices2str.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 #
-# Example script to take V86MMGR.INC and dump all V86MMGR_Service enumerations
-#file=/mnt/main/emu/win31ddk/DDK/386/include/v86mmgr.inc
-#file=~/Downloads/x/ddk95/Inc32/V86MMGR.INC
-#file=~/Downloads/win98ddk/98ddk/inc/win98/V86MMGR.INC
-file=~/Downloads/winmeddk/NTDDK/inc/win_me/v86mmgr.inc
+# Example script to take PAGESWAP.INC and dump all PAGESWAP_Service enumerations
+#file=/mnt/main/emu/win31ddk/DDK/386/include/pageswap.inc
+#file=~/Downloads/x/ddk95/Inc32/PAGESWAP.INC
+#file=~/Downloads/win98ddk/98ddk/inc/win98/PAGESWAP.INC
+file=~/Downloads/winmeddk/NTDDK/inc/win_me/pageswap.inc
 count=0
 
 ver="4.90+"
-device=0x0006
+device=0x0007
 
-grep -R "V86MMGR_Service" $file | cut -d $'\t' -f 2 | cut -d ',' -f 1 | sed -e 's/\x0D//g' | sed -e "s/V86MMGR_Service *//" | while read X; do
+grep -R "PageSwap_Service" $file | cut -d $'\t' -f 2 | cut -d ',' -f 1 | sed -e 's/\x0D//g' | sed -e "s/PageSwap_Service *//" | while read X; do
     counth=`printf '%-8s %04XH %04XH  %s' "$ver" $device $count "$X"`;
     echo "$counth"
     count=$(($count+1))
