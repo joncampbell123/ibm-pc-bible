af9938fb93606eebe59d42ea284470ab88330b2e
Jonathan Campbell <jonathan@castus.tv>
2017-02-13 22:45:34 -0800

    more

--- ddkincservices2str.sh
+++ ddkincservices2str.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 #
-# Example script to take VSD.INC and dump all VSD_Service enumerations
-#file=/mnt/main/emu/win31ddk/DDK/386/include/vsd.inc
-#file=~/Downloads/x/ddk95/Inc32/VSD.INC
-#file=~/Downloads/win98ddk/98ddk/inc/win98/VSD.INC
-file=~/Downloads/winmeddk/NTDDK/inc/win_me/vsd.inc
+# Example script to take VMD.INC and dump all VMOUSE_Service enumerations
+#file=/mnt/main/emu/win31ddk/DDK/386/include/vmd.inc
+#file=~/Downloads/x/ddk95/Inc32/VMD.INC
+#file=~/Downloads/win98ddk/98ddk/inc/win98/VMD.INC
+file=~/Downloads/winmeddk/NTDDK/inc/win_me/vmd.inc
 count=0
 
 ver="4.90+"
-device=0x000B
+device=0x000C
 
-grep -R "VSD_Service" $file | cut -d $'\t' -f 2 | cut -d ',' -f 1 | sed -e 's/\x0D//g' | sed -e "s/VSD_Service *//" | while read X; do
+grep -R "VMOUSE_Service" $file | cut -d $'\t' -f 2 | cut -d ',' -f 1 | sed -e 's/\x0D//g' | sed -e "s/VMOUSE_Service *//" | while read X; do
     counth=`printf '%-8s %04XH %04XH  %s' "$ver" $device $count "$X"`;
     echo "$counth"
     count=$(($count+1))
