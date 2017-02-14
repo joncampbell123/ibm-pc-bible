d7bd11b7bfc480b7ab342fd052c4e77de18f42ae
Jonathan Campbell <jonathan@castus.tv>
2017-02-14 09:35:02 -0800

    VPOWERD, adjust ddkincservice2str.sh to handle variations in tab/indent in VPOWERD.INC

--- ddkincservices2str.sh
+++ ddkincservices2str.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 #
-# Example script to take PAGEFILE.INC and dump all PAGEFILE_Service enumerations
-#file=/mnt/main/emu/win31ddk/DDK/386/include/pagefile.inc
-#file=~/Downloads/x/ddk95/Inc32/PAGEFILE.INC
-#file=~/Downloads/win98ddk/98ddk/inc/win98/PAGEFILE.INC
-file=~/Downloads/winmeddk/NTDDK/inc/win_me/pagefile.inc
+# Example script to take VPOWERD.INC and dump all VPOWERD_Service enumerations
+#file=/mnt/main/emu/win31ddk/DDK/386/include/vpowerd.inc
+#file=~/Downloads/x/ddk95/Inc32/VPOWERD.INC
+#file=~/Downloads/win98ddk/98ddk/inc/win98/VPOWERD.INC
+file=~/Downloads/winmeddk/NTDDK/inc/win_me/vpowerd.inc
 count=0
 
 ver="4.90+"
-device=0x0021
+device=0x0026
 
-grep -R "PageFile_Service" $file | sed "s/ \+/"$'\t'"/g" | cut -d $'\t' -f 2 | cut -d ',' -f 1 | sed -e 's/\x0D//g' | sed -e "s/PageFile_Service *//" | while read X; do
+grep -R "VPOWERD_Service" $file | sed "s/^[\t ]*//" | sed "s/[\t ]\+/"$'\t'"/g" | cut -d $'\t' -f 2 | cut -d ',' -f 1 | sed -e 's/\x0D//g' | sed -e "s/VPOWERD_Service *//" | while read X; do
     counth=`printf '%-8s %04XH %04XH  %s' "$ver" $device $count "$X"`;
     echo "$counth"
     count=$(($count+1))
