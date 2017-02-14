df415fd5f203d51c5f3935eb9a631831be13f36a
Jonathan Campbell <jonathan@castus.tv>
2017-02-13 23:04:07 -0800

    BLOCKDEV symbols

--- ddkincservices2str.sh
+++ ddkincservices2str.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 #
-# Example script to take VCD.INC and dump all VCD_Service enumerations
-#file=/mnt/main/emu/win31ddk/DDK/386/include/vcd.inc
-#file=~/Downloads/x/ddk95/Inc32/VCD.INC
-#file=~/Downloads/win98ddk/98ddk/inc/win98/VCD.INC
-file=~/Downloads/winmeddk/NTDDK/inc/win_me/vcd.inc
+# Example script to take BLOCKDEV.INC and dump all BlockDev_Service enumerations
+#file=/mnt/main/emu/win31ddk/DDK/386/include/blockdev.inc
+#file=~/Downloads/x/ddk95/Inc32/BLOCKDEV.INC
+#file=~/Downloads/win98ddk/98ddk/inc/win98/BLOCKDEV.INC
+file=~/Downloads/winmeddk/NTDDK/inc/win_me/blockdev.inc
 count=0
 
 ver="4.90+"
-device=0x000E
+device=0x0010
 
-grep -R "VCD_Service" $file | cut -d $'\t' -f 2 | cut -d ',' -f 1 | sed -e 's/\x0D//g' | sed -e "s/VCD_Service *//" | while read X; do
+grep -R "BlockDev_Service" $file | cut -d $'\t' -f 2 | cut -d ',' -f 1 | sed -e 's/\x0D//g' | sed -e "s/BlockDev_Service *//" | while read X; do
     counth=`printf '%-8s %04XH %04XH  %s' "$ver" $device $count "$X"`;
     echo "$counth"
     count=$(($count+1))
