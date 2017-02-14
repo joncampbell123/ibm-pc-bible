2aac7e18115c988a343b43885efa0e7577ec4d03
Jonathan Campbell <jonathan@castus.tv>
2017-02-14 09:39:09 -0800

    VXDLDR. This appears to be Win95's dynamic VXD loader. Based on the symbols listed I'm guessing this is also where Win32 loading is done.

--- ddkincservices2str.sh
+++ ddkincservices2str.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 #
-# Example script to take VPOWERD.INC and dump all VPOWERD_Service enumerations
-#file=/mnt/main/emu/win31ddk/DDK/386/include/vpowerd.inc
-#file=~/Downloads/x/ddk95/Inc32/VPOWERD.INC
-#file=~/Downloads/win98ddk/98ddk/inc/win98/VPOWERD.INC
-file=~/Downloads/winmeddk/NTDDK/inc/win_me/vpowerd.inc
+# Example script to take VXDLDR.INC and dump all VXDLDR_Service enumerations
+#file=/mnt/main/emu/win31ddk/DDK/386/include/vxdldr.inc
+#file=~/Downloads/x/ddk95/Inc32/VXDLDR.INC
+#file=~/Downloads/win98ddk/98ddk/inc/win98/VXDLDR.INC
+file=~/Downloads/winmeddk/NTDDK/inc/win_me/vxdldr.inc
 count=0
 
 ver="4.90+"
-device=0x0026
+device=0x0027
 
-grep -R "VPOWERD_Service" $file | sed "s/^[\t ]*//" | sed "s/[\t ]\+/"$'\t'"/g" | cut -d $'\t' -f 2 | cut -d ',' -f 1 | sed -e 's/\x0D//g' | sed -e "s/VPOWERD_Service *//" | while read X; do
+grep -R "VXDLDR_Service" $file | sed "s/^[\t ]*//" | sed "s/[\t ]\+/"$'\t'"/g" | cut -d $'\t' -f 2 | cut -d ',' -f 1 | sed -e 's/\x0D//g' | sed -e "s/VXDLDR_Service *//" | while read X; do
     counth=`printf '%-8s %04XH %04XH  %s' "$ver" $device $count "$X"`;
     echo "$counth"
     count=$(($count+1))
