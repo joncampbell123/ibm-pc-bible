34b9f227c4f02975c9dbc1b6c21a32d501ad51ba
Jonathan Campbell <jonathan@castus.tv>
2017-02-13 23:13:34 -0800

    more

--- ddkincservices2str.sh
+++ ddkincservices2str.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 #
-# Example script to take EBIOS.INC and dump all EBIOS_Service enumerations
-#file=/mnt/main/emu/win31ddk/DDK/386/include/ebios.inc
-#file=~/Downloads/x/ddk95/Inc32/EBIOS.INC
-#file=~/Downloads/win98ddk/98ddk/inc/win98/EBIOS.INC
-file=~/Downloads/winmeddk/NTDDK/inc/win_me/ebios.inc
+# Example script to take BIOSXlat.INC and dump all BIOSXlat_Service enumerations
+#file=/mnt/main/emu/win31ddk/DDK/386/include/biosxlat.inc
+#file=~/Downloads/x/ddk95/Inc32/BIOSXLAT.INC
+#file=~/Downloads/win98ddk/98ddk/inc/win98/BIOSXLAT.INC
+#file=~/Downloads/winmeddk/NTDDK/inc/win_me/biosxlat.inc
 count=0
 
-ver="4.90+"
-device=0x0012
+ver="3.1+"
+device=0x0013
 
-grep -R "EBIOS_Service" $file | cut -d $'\t' -f 2 | cut -d ',' -f 1 | sed -e 's/\x0D//g' | sed -e "s/EBIOS_Service *//" | while read X; do
+grep -R "BIOSXlat_Service" $file | cut -d $'\t' -f 2 | cut -d ',' -f 1 | sed -e 's/\x0D//g' | sed -e "s/BIOSXlat_Service *//" | while read X; do
     counth=`printf '%-8s %04XH %04XH  %s' "$ver" $device $count "$X"`;
     echo "$counth"
     count=$(($count+1))
