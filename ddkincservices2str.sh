bc64a12fae1e8cdacdf3fad0e9ed74bada50510e
Jonathan Campbell <jonathan@castus.tv>
2017-02-14 09:10:24 -0800

    more

--- ddkincservices2str.sh
+++ ddkincservices2str.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 #
-# Example script to take BIOSHOOK.INC and dump all BIOSHOOK_Service enumerations
-file=/mnt/main/emu/win31ddk/DDK/386/include/bioshook.inc
-#file=~/Downloads/x/ddk95/Inc32/BIOSHOOK.INC
-#file=~/Downloads/win98ddk/98ddk/inc/win98/BIOSHOOK.INC
-#file=~/Downloads/winmeddk/NTDDK/inc/win_me/bioshook.inc
+# Example script to take INT13.INC and dump all INT13_Service enumerations
+#file=/mnt/main/emu/win31ddk/DDK/386/include/int13.inc
+#file=~/Downloads/x/ddk95/Inc32/INT13.INC
+#file=~/Downloads/win98ddk/98ddk/inc/win98/INT13.INC
+file=~/Downloads/winmeddk/NTDDK/inc/win_me/int13.inc
 count=0
 
-ver="3.1+"
-device=0x001F
+ver="4.90+"
+device=0x0020
 
-grep -R "BiosHook_Service" $file | cut -d $'\t' -f 2 | cut -d ',' -f 1 | sed -e 's/\x0D//g' | sed -e "s/BiosHook_Service *//" | while read X; do
+grep -R "Int13_Service" $file | cut -d $'\t' -f 2 | cut -d ',' -f 1 | sed -e 's/\x0D//g' | sed -e "s/Int13_Service *//" | while read X; do
     counth=`printf '%-8s %04XH %04XH  %s' "$ver" $device $count "$X"`;
     echo "$counth"
     count=$(($count+1))
