490a6773858469830b037ff7de3d811212310e4e
Jonathan Campbell <jonathan@castus.tv>
2017-02-13 22:37:03 -0800

    VDD symbols. Apparently Microsoft changed the meaning of at least one symbol between Win95 and Win98, eugh!

--- ddkincservices2str.sh
+++ ddkincservices2str.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 #
-# Example script to take PAGESWAP.INC and dump all PAGESWAP_Service enumerations
-#file=/mnt/main/emu/win31ddk/DDK/386/include/pageswap.inc
-#file=~/Downloads/x/ddk95/Inc32/PAGESWAP.INC
-#file=~/Downloads/win98ddk/98ddk/inc/win98/PAGESWAP.INC
-file=~/Downloads/winmeddk/NTDDK/inc/win_me/pageswap.inc
+# Example script to take VDD.INC and dump all VDD_Service enumerations
+#file=/mnt/main/emu/win31ddk/DDK/386/include/vdd.inc
+#file=~/Downloads/x/ddk95/Inc32/MINIVDD.INC
+#file=~/Downloads/win98ddk/98ddk/inc/win98/MINIVDD.INC
+file=~/Downloads/winmeddk/NTDDK/inc/win_me/minivdd.inc
 count=0
 
 ver="4.90+"
-device=0x0007
+device=0x000A
 
-grep -R "PageSwap_Service" $file | cut -d $'\t' -f 2 | cut -d ',' -f 1 | sed -e 's/\x0D//g' | sed -e "s/PageSwap_Service *//" | while read X; do
+grep -R "VDD_Service" $file | cut -d $'\t' -f 2 | cut -d ',' -f 1 | sed -e 's/\x0D//g' | sed -e "s/VDD_Service *//" | while read X; do
     counth=`printf '%-8s %04XH %04XH  %s' "$ver" $device $count "$X"`;
     echo "$counth"
     count=$(($count+1))
