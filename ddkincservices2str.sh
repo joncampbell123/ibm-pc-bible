aca7e7f274c4e6a07bc1494311c280058a9ed75f
Jonathan Campbell <jonathan@castus.tv>
2017-02-14 09:47:19 -0800

    VWIN32

--- ddkincservices2str.sh
+++ ddkincservices2str.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 #
-# Example script to take NDIS.INC and dump all Ndis_Service enumerations
-#file=/mnt/main/emu/win31ddk/DDK/386/include/ndis.inc
-#file=~/Downloads/x/ddk95/Inc32/NDIS.H
-#file=~/Downloads/win98ddk/98ddk/inc/win98/NDIS.INC
-file=~/Downloads/winmeddk/NTDDK/inc/win_me/ndis.inc
+# Example script to take VWIN32.INC and dump all VWIN32_Service enumerations
+#file=/mnt/main/emu/win31ddk/DDK/386/include/vwin32.inc
+#file=~/Downloads/x/ddk95/Inc32/VWIN32.INC
+#file=~/Downloads/win98ddk/98ddk/inc/win98/VWIN32.INC
+file=~/Downloads/winmeddk/NTDDK/inc/win_me/vwin32.inc
 count=0
 
 ver="4.90+"
-device=0x0028
+device=0x002A
 
-grep -R "Ndis_Service" $file | sed "s/^[\t ]*//" | sed "s/[\t ]\+/"$'\t'"/g" | cut -d $'\t' -f 2 | cut -d ',' -f 1 | sed -e 's/\x0D//g' | sed -e "s/Ndis_Service *//" | while read X; do
+grep -R "VWIN32_Service" $file | sed "s/^[\t ]*//" | sed "s/[\t ]\+/"$'\t'"/g" | cut -d $'\t' -f 2 | cut -d ',' -f 1 | sed -e 's/\x0D//g' | sed -e "s/VWIN32_Service *//" | while read X; do
     counth=`printf '%-8s %04XH %04XH  %s' "$ver" $device $count "$X"`;
     echo "$counth"
     count=$(($count+1))
