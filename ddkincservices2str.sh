acbdcdac50c76310c1a4f5f73a4956ec249d9b8c
Jonathan Campbell <jonathan@castus.tv>
2017-02-13 23:29:55 -0800

    more

--- ddkincservices2str.sh
+++ ddkincservices2str.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 #
-# Example script to take VFD.INC and dump all VFD_Service enumerations
-file=/mnt/main/emu/win31ddk/DDK/386/include/vfd.inc
-#file=~/Downloads/x/ddk95/Inc32/VFD.INC
-#file=~/Downloads/win98ddk/98ddk/inc/win98/VFD.INC
-#file=~/Downloads/winmeddk/NTDDK/inc/win_me/vfd.inc
+# Example script to take VDD2.INC and dump all VDD2_Service enumerations
+#file=/mnt/main/emu/win31ddk/DDK/386/include/vdd2.inc
+#file=~/Downloads/x/ddk95/Inc32/VDD2.INC
+#file=~/Downloads/win98ddk/98ddk/inc/win98/VDD2.INC
+file=~/Downloads/winmeddk/NTDDK/inc/win_me/vdd2.inc
 count=0
 
-ver="3.1+"
-device=0x001B
+ver="4.90+"
+device=0x001C
 
-grep -R "VFD_Service" $file | cut -d $'\t' -f 2 | cut -d ',' -f 1 | sed -e 's/\x0D//g' | sed -e "s/VFD_Service *//" | while read X; do
+grep -R "VDD2_Service" $file | cut -d $'\t' -f 2 | cut -d ',' -f 1 | sed -e 's/\x0D//g' | sed -e "s/VDD2_Service *//" | while read X; do
     counth=`printf '%-8s %04XH %04XH  %s' "$ver" $device $count "$X"`;
     echo "$counth"
     count=$(($count+1))
