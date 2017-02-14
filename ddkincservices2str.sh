951ce68dc402b67ae3040b87db360a78d0382fd1
Jonathan Campbell <jonathan@castus.tv>
2017-02-14 09:43:10 -0800

    NDIS VXD services. Amusing note: Someone in the Windows 95 DDK thought it would be clever to include the ASM service list in the C header file.

--- ddkincservices2str.sh
+++ ddkincservices2str.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 #
-# Example script to take VXDLDR.INC and dump all VXDLDR_Service enumerations
-#file=/mnt/main/emu/win31ddk/DDK/386/include/vxdldr.inc
-#file=~/Downloads/x/ddk95/Inc32/VXDLDR.INC
-#file=~/Downloads/win98ddk/98ddk/inc/win98/VXDLDR.INC
-file=~/Downloads/winmeddk/NTDDK/inc/win_me/vxdldr.inc
+# Example script to take NDIS.INC and dump all Ndis_Service enumerations
+#file=/mnt/main/emu/win31ddk/DDK/386/include/ndis.inc
+#file=~/Downloads/x/ddk95/Inc32/NDIS.H
+#file=~/Downloads/win98ddk/98ddk/inc/win98/NDIS.INC
+file=~/Downloads/winmeddk/NTDDK/inc/win_me/ndis.inc
 count=0
 
 ver="4.90+"
-device=0x0027
+device=0x0028
 
-grep -R "VXDLDR_Service" $file | sed "s/^[\t ]*//" | sed "s/[\t ]\+/"$'\t'"/g" | cut -d $'\t' -f 2 | cut -d ',' -f 1 | sed -e 's/\x0D//g' | sed -e "s/VXDLDR_Service *//" | while read X; do
+grep -R "Ndis_Service" $file | sed "s/^[\t ]*//" | sed "s/[\t ]\+/"$'\t'"/g" | cut -d $'\t' -f 2 | cut -d ',' -f 1 | sed -e 's/\x0D//g' | sed -e "s/Ndis_Service *//" | while read X; do
     counth=`printf '%-8s %04XH %04XH  %s' "$ver" $device $count "$X"`;
     echo "$counth"
     count=$(($count+1))
