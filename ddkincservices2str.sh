892e3593cf31a2a9b2435adc6978bbfaaba9a29f
Jonathan Campbell <jonathan@castus.tv>
2017-02-13 21:05:55 -0800

    more

---
+++ ddkincservices2str.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#
+# Example script to take VMM.INC and dump all VMM_Service enumerations
+file=/mnt/main/emu/win31ddk/DDK/386/include/vmm.inc
+#file=~/Downloads/x/ddk95/Inc32/VMM.INC
+count=0
+
+ver="3.0+"
+device=0x0001
+
+grep -R "VMM_Service" $file | cut -d $'\t' -f 2 | cut -d ',' -f 1 | sed -e 's/\x0D//g' | while read X; do
+    counth=`printf '%-8s %04XH %04XH  %s' $ver $device $count "$X"`;
+    echo "$counth"
+    count=$(($count+1))
+done
+
