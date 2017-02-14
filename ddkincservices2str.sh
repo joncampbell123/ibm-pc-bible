e8c723e9605740e7df51a024b24f49da870605f1
Jonathan Campbell <jonathan@castus.tv>
2017-02-13 23:17:42 -0800

    DOSMGR

--- ddkincservices2str.sh
+++ ddkincservices2str.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 #
-# Example script to take VNETBIOS.INC and dump all VNETBIOS_Service enumerations
-#file=/mnt/main/emu/win31ddk/DDK/386/include/vnetbios.inc
-#file=~/Downloads/x/ddk95/Inc32/VNETBIOS.INC
-#file=~/Downloads/win98ddk/98ddk/inc/win98/VNETBIOS.INC
-file=~/Downloads/winmeddk/NTDDK/inc/win_me/vnetbios.inc
+# Example script to take DOSMGR.INC and dump all DOSMGR_Service enumerations
+#file=/mnt/main/emu/win31ddk/DDK/386/include/dosmgr.inc
+#file=~/Downloads/x/ddk95/Inc32/DOSMGR.INC
+#file=~/Downloads/win98ddk/98ddk/inc/win98/DOSMGR.INC
+file=~/Downloads/winmeddk/NTDDK/inc/win_me/dosmgr.inc
 count=0
 
 ver="4.90+"
-device=0x0014
+device=0x0015
 
-grep -R "VNETBIOS_Service" $file | cut -d $'\t' -f 2 | cut -d ',' -f 1 | sed -e 's/\x0D//g' | sed -e "s/VNETBIOS_Service *//" | while read X; do
+grep -R "DOSMGR_Service" $file | cut -d $'\t' -f 2 | cut -d ',' -f 1 | sed -e 's/\x0D//g' | sed -e "s/DOSMGR_Service *//" | while read X; do
     counth=`printf '%-8s %04XH %04XH  %s' "$ver" $device $count "$X"`;
     echo "$counth"
     count=$(($count+1))
