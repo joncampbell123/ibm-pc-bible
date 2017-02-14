08a554b03943d4a6f6019391cdb84d7abdc73a02
Jonathan Campbell <jonathan@castus.tv>
2017-02-13 23:19:42 -0800

    more

--- ddkincservices2str.sh
+++ ddkincservices2str.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 #
-# Example script to take DOSMGR.INC and dump all DOSMGR_Service enumerations
-#file=/mnt/main/emu/win31ddk/DDK/386/include/dosmgr.inc
-#file=~/Downloads/x/ddk95/Inc32/DOSMGR.INC
-#file=~/Downloads/win98ddk/98ddk/inc/win98/DOSMGR.INC
-file=~/Downloads/winmeddk/NTDDK/inc/win_me/dosmgr.inc
+# Example script to take WINLOAD.INC and dump all WINLOAD_Service enumerations
+file=/mnt/main/emu/win31ddk/DDK/386/include/winload.inc
+#file=~/Downloads/x/ddk95/Inc32/WINLOAD.INC
+#file=~/Downloads/win98ddk/98ddk/inc/win98/WINLOAD.INC
+#file=~/Downloads/winmeddk/NTDDK/inc/win_me/winload.inc
 count=0
 
-ver="4.90+"
-device=0x0015
+ver="3.1+"
+device=0x0016
 
-grep -R "DOSMGR_Service" $file | cut -d $'\t' -f 2 | cut -d ',' -f 1 | sed -e 's/\x0D//g' | sed -e "s/DOSMGR_Service *//" | while read X; do
+grep -R "WINLOAD_Service" $file | cut -d $'\t' -f 2 | cut -d ',' -f 1 | sed -e 's/\x0D//g' | sed -e "s/WINLOAD_Service *//" | while read X; do
     counth=`printf '%-8s %04XH %04XH  %s' "$ver" $device $count "$X"`;
     echo "$counth"
     count=$(($count+1))
