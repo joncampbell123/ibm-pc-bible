aeca7f8fa0f8a2c9b1613f10aec8aa0a73f7bb34
Jonathan Campbell <jonathan@castus.tv>
2017-02-13 23:30:49 -0800

    more

--- ddkincservices2str.sh
+++ ddkincservices2str.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 #
-# Example script to take VDD2.INC and dump all VDD2_Service enumerations
-#file=/mnt/main/emu/win31ddk/DDK/386/include/vdd2.inc
-#file=~/Downloads/x/ddk95/Inc32/VDD2.INC
-#file=~/Downloads/win98ddk/98ddk/inc/win98/VDD2.INC
-file=~/Downloads/winmeddk/NTDDK/inc/win_me/vdd2.inc
+# Example script to take WINDEBUG.INC and dump all WINDEBUG_Service enumerations
+#file=/mnt/main/emu/win31ddk/DDK/386/include/windebug.inc
+file=~/Downloads/x/ddk95/Inc32/WINDEBUG.INC
+#file=~/Downloads/win98ddk/98ddk/inc/win98/WINDEBUG.INC
+#file=~/Downloads/winmeddk/NTDDK/inc/win_me/windebug.inc
 count=0
 
-ver="4.90+"
-device=0x001C
+ver="4.0+"
+device=0x001D
 
-grep -R "VDD2_Service" $file | cut -d $'\t' -f 2 | cut -d ',' -f 1 | sed -e 's/\x0D//g' | sed -e "s/VDD2_Service *//" | while read X; do
+grep -R "WINDEBUG_Service" $file | cut -d $'\t' -f 2 | cut -d ',' -f 1 | sed -e 's/\x0D//g' | sed -e "s/WINDEBUG_Service *//" | while read X; do
     counth=`printf '%-8s %04XH %04XH  %s' "$ver" $device $count "$X"`;
     echo "$counth"
     count=$(($count+1))
