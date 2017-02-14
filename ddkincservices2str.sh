dadf96a87cd24c515fa4eaabbd9e10dfe14e521e
Jonathan Campbell <jonathan@castus.tv>
2017-02-14 15:42:11 -0800

    vfbackup win95 thing

--- ddkincservices2str.sh
+++ ddkincservices2str.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 #
-# Example script to take CONFIGMG.INC and dump all CONFIGMG_Service enumerations
-#file=/mnt/main/emu/win31ddk/DDK/386/include/configmg.inc
-#file=~/Downloads/x/ddk95/Inc32/CONFIGMG.INC
-#file=~/Downloads/win98ddk/98ddk/inc/win98/CONFIGMG.INC
-file=~/Downloads/winmeddk/NTDDK/inc/win_me/configmg.inc
+# Example script to take VFBACKUP.INC and dump all VFBACKUP_Service enumerations
+#file=/mnt/main/emu/win31ddk/DDK/386/include/vfbackup.inc
+#file=~/Downloads/x/ddk95/Inc32/VFBACKUP.INC
+#file=~/Downloads/win98ddk/98ddk/inc/win98/VFBACKUP.INC
+file=~/Downloads/winmeddk/NTDDK/inc/win_me/vfbackup.inc
 count=0
 
 ver="4.90+"
-device=0x0033
+device=0x0036
 
-grep -R "CONFIGMG_Service" $file | sed "s/^[\t ]*//" | sed "s/[\t ]\+/"$'\t'"/g" | cut -d $'\t' -f 2 | cut -d ',' -f 1 | sed -e 's/\x0D//g' | sed -e "s/CONFIGMG_Service *//" | while read X; do
+grep -R "VFBACKUP_Service" $file | sed "s/^[\t ]*//" | sed "s/[\t ]\+/"$'\t'"/g" | cut -d $'\t' -f 2 | cut -d ',' -f 1 | sed -e 's/\x0D//g' | sed -e "s/VFBACKUP_Service *//" | while read X; do
     counth=`printf '%-8s %04XH %04XH  %s' "$ver" $device $count "$X"`;
     echo "$counth"
     count=$(($count+1))
