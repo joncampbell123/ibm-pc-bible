e83b822fd371304b30b8dfc739633efcecc4546b
Jonathan Campbell <jonathan@castus.tv>
2017-02-14 15:45:14 -0800

    VCOND

--- ddkincservices2str.sh
+++ ddkincservices2str.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 #
-# Example script to take ENABLE.INC and dump all ENABLE_Service enumerations
-#file=/mnt/main/emu/win31ddk/DDK/386/include/enable.inc
-#file=~/Downloads/x/ddk95/Inc32/ENABLE.INC
-#file=~/Downloads/win98ddk/98ddk/inc/win98/ENABLE.INC
-file=~/Downloads/winmeddk/NTDDK/inc/win_me/enable.inc
+# Example script to take VCOND.INC and dump all VCOND_Service enumerations
+#file=/mnt/main/emu/win31ddk/DDK/386/include/vcond.inc
+#file=~/Downloads/x/ddk95/Inc32/VCOND.INC
+#file=~/Downloads/win98ddk/98ddk/inc/win98/VCOND.INC
+file=~/Downloads/winmeddk/NTDDK/inc/win_me/vcond.inc
 count=0
 
 ver="4.90+"
-device=0x0037
+device=0x0038
 
-grep -R "ENABLE_Service" $file | sed "s/^[\t ]*//" | sed "s/[\t ]\+/"$'\t'"/g" | cut -d $'\t' -f 2 | cut -d ',' -f 1 | sed -e 's/\x0D//g' | sed -e "s/ENABLE_Service *//" | while read X; do
+grep -R "VCOND_Service" $file | sed "s/^[\t ]*//" | sed "s/[\t ]\+/"$'\t'"/g" | cut -d $'\t' -f 2 | cut -d ',' -f 1 | sed -e 's/\x0D//g' | sed -e "s/VCOND_Service *//" | while read X; do
     counth=`printf '%-8s %04XH %04XH  %s' "$ver" $device $count "$X"`;
     echo "$counth"
     count=$(($count+1))
