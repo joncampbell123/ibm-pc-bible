9e0d32a6492aa25a92a7a22e762f26ae6ab4808e
Jonathan Campbell <jonathan@castus.tv>
2017-02-13 23:27:22 -0800

    DOSNET

--- ddkincservices2str.sh
+++ ddkincservices2str.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 #
-# Example script to take VPROD.INC and dump all VPROD_Service enumerations
-file=/mnt/main/emu/win31ddk/DDK/386/include/vprod.inc
-#file=~/Downloads/x/ddk95/Inc32/VPROD.INC
-#file=~/Downloads/win98ddk/98ddk/inc/win98/VPROD.INC
-#file=~/Downloads/winmeddk/NTDDK/inc/win_me/vprod.inc
+# Example script to take DOSNET.INC and dump all DOSNET_Service enumerations
+#file=/mnt/main/emu/win31ddk/DDK/386/include/dosnet.inc
+#file=~/Downloads/x/ddk95/Inc32/DOSNET.INC
+#file=~/Downloads/win98ddk/98ddk/inc/win98/DOSNET.INC
+file=~/Downloads/winmeddk/NTDDK/inc/win_me/dosnet.inc
 count=0
 
-ver="3.1+"
-device=0x0019
+ver="4.90+"
+device=0x001A
 
-grep -R "VPROD_Service" $file | cut -d $'\t' -f 2 | cut -d ',' -f 1 | sed -e 's/\x0D//g' | sed -e "s/VPROD_Service *//" | while read X; do
+grep -R "DOSNET_Service" $file | cut -d $'\t' -f 2 | cut -d ',' -f 1 | sed -e 's/\x0D//g' | sed -e "s/DOSNET_Service *//" | while read X; do
     counth=`printf '%-8s %04XH %04XH  %s' "$ver" $device $count "$X"`;
     echo "$counth"
     count=$(($count+1))
