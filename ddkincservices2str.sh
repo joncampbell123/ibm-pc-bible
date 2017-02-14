d89c6eca3328978dbbf73fb51845e4ab6e2875f6
Jonathan Campbell <jonathan@castus.tv>
2017-02-13 23:24:08 -0800

    VMPOLL

--- ddkincservices2str.sh
+++ ddkincservices2str.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 #
-# Example script to take SHELL.INC and dump all SHELL_Service enumerations
-#file=/mnt/main/emu/win31ddk/DDK/386/include/shell.inc
-#file=~/Downloads/x/ddk95/Inc32/SHELL.INC
-#file=~/Downloads/win98ddk/98ddk/inc/win98/SHELL.INC
-file=~/Downloads/winmeddk/NTDDK/inc/win_me/shell.inc
+# Example script to take VMPoll.INC and dump all VMPoll_Service enumerations
+#file=/mnt/main/emu/win31ddk/DDK/386/include/vmpoll.inc
+#file=~/Downloads/x/ddk95/Inc32/VMPOLL.INC
+#file=~/Downloads/win98ddk/98ddk/inc/win98/VMPOLL.INC
+file=~/Downloads/winmeddk/NTDDK/inc/win_me/vmpoll.inc
 count=0
 
 ver="4.90+"
-device=0x0017
+device=0x0018
 
-grep -R "SHELL_Service" $file | cut -d $'\t' -f 2 | cut -d ',' -f 1 | sed -e 's/\x0D//g' | sed -e "s/SHELL_Service *//" | while read X; do
+grep -R "VMPoll_Service" $file | cut -d $'\t' -f 2 | cut -d ',' -f 1 | sed -e 's/\x0D//g' | sed -e "s/VMPoll_Service *//" | while read X; do
     counth=`printf '%-8s %04XH %04XH  %s' "$ver" $device $count "$X"`;
     echo "$counth"
     count=$(($count+1))
