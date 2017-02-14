6c6447b4a9a9c7c11fc6f8a46c68c73bb8accb9b
Jonathan Campbell <jonathan@castus.tv>
2017-02-13 22:01:33 -0800

    finally, VMM Windows ME symbols

--- ddkincservices2str.sh
+++ ddkincservices2str.sh
@@ -3,10 +3,11 @@
 # Example script to take VMM.INC and dump all VMM_Service enumerations
 #file=/mnt/main/emu/win31ddk/DDK/386/include/vmm.inc
 #file=~/Downloads/x/ddk95/Inc32/VMM.INC
-file=~/Downloads/win98ddk/98ddk/inc/win98/VMM.INC
+#file=~/Downloads/win98ddk/98ddk/inc/win98/VMM.INC
+file=~/Downloads/winmeddk/NTDDK/inc/win_me/vmm.inc
 count=0
 
-ver="4.10+"
+ver="4.90+"
 device=0x0001
 
 grep -R "VMM_Service" $file | cut -d $'\t' -f 2 | cut -d ',' -f 1 | sed -e 's/\x0D//g' | sed -e "s/VMM_Service *//" | while read X; do
