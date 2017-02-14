4ae0c5861a917c7088cd448386f39d1ba03fe0c0
Jonathan Campbell <jonathan@castus.tv>
2017-02-13 23:21:46 -0800

    SHELL

--- ddkincservices2str.sh
+++ ddkincservices2str.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 #
-# Example script to take WINLOAD.INC and dump all WINLOAD_Service enumerations
-file=/mnt/main/emu/win31ddk/DDK/386/include/winload.inc
-#file=~/Downloads/x/ddk95/Inc32/WINLOAD.INC
-#file=~/Downloads/win98ddk/98ddk/inc/win98/WINLOAD.INC
-#file=~/Downloads/winmeddk/NTDDK/inc/win_me/winload.inc
+# Example script to take SHELL.INC and dump all SHELL_Service enumerations
+#file=/mnt/main/emu/win31ddk/DDK/386/include/shell.inc
+#file=~/Downloads/x/ddk95/Inc32/SHELL.INC
+#file=~/Downloads/win98ddk/98ddk/inc/win98/SHELL.INC
+file=~/Downloads/winmeddk/NTDDK/inc/win_me/shell.inc
 count=0
 
-ver="3.1+"
-device=0x0016
+ver="4.90+"
+device=0x0017
 
-grep -R "WINLOAD_Service" $file | cut -d $'\t' -f 2 | cut -d ',' -f 1 | sed -e 's/\x0D//g' | sed -e "s/WINLOAD_Service *//" | while read X; do
+grep -R "SHELL_Service" $file | cut -d $'\t' -f 2 | cut -d ',' -f 1 | sed -e 's/\x0D//g' | sed -e "s/SHELL_Service *//" | while read X; do
     counth=`printf '%-8s %04XH %04XH  %s' "$ver" $device $count "$X"`;
     echo "$counth"
     count=$(($count+1))
