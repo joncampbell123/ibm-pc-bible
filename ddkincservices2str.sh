00fcb0c727d2c335b08ca600e2afefd0d2c8d7eb
Jonathan Campbell <jonathan@castus.tv>
2017-02-13 22:08:05 -0800

    more

--- ddkincservices2str.sh
+++ ddkincservices2str.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 #
-# Example script to take VMM.INC and dump all VMM_Service enumerations
-#file=/mnt/main/emu/win31ddk/DDK/386/include/vmm.inc
-#file=~/Downloads/x/ddk95/Inc32/VMM.INC
-#file=~/Downloads/win98ddk/98ddk/inc/win98/VMM.INC
-file=~/Downloads/winmeddk/NTDDK/inc/win_me/vmm.inc
+# Example script to take VMM.INC and dump all DEBUG_Service enumerations
+#file=/mnt/main/emu/win31ddk/DDK/386/include/debug.inc
+#file=~/Downloads/x/ddk95/Inc32/DEBUG.INC
+#file=~/Downloads/win98ddk/98ddk/inc/win98/DEBUG.INC
+file=~/Downloads/winmeddk/NTDDK/inc/win_me/debug.inc
 count=0
 
 ver="4.90+"
-device=0x0001
+device=0x0002
 
-grep -R "VMM_Service" $file | cut -d $'\t' -f 2 | cut -d ',' -f 1 | sed -e 's/\x0D//g' | sed -e "s/VMM_Service *//" | while read X; do
+grep -R "DEBUG_Service" $file | cut -d $'\t' -f 2 | cut -d ',' -f 1 | sed -e 's/\x0D//g' | sed -e "s/DEBUG_Service *//" | while read X; do
     counth=`printf '%-8s %04XH %04XH  %s' "$ver" $device $count "$X"`;
     echo "$counth"
     count=$(($count+1))
