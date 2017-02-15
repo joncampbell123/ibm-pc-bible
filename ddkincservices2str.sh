1d02db2c3fb48a8a1cdc10ba88ceaf11652182a6
Jonathan Campbell <jonathan@castus.tv>
2017-02-14 16:19:08 -0800

    multimedia VxD list, begin

--- ddkincservices2str.sh
+++ ddkincservices2str.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 #
-# Example script to take IOS.INC and dump all IOS_Service enumerations
-#file=/mnt/main/emu/win31ddk/DDK/386/include/ios.inc
-#file=~/Downloads/x/ddk95/Inc32/IOS.INC
-#file=~/Downloads/win98ddk/98ddk/inc/win98/IOS.INC
-file=~/Downloads/winmeddk/NTDDK/inc/win_me/ios.inc
+# Example script to take MMDEVLDR.INC and dump all MMDEVLDR_Service enumerations
+#file=/mnt/main/emu/win31ddk/DDK/386/include/mmdevldr.inc
+#file=~/Downloads/x/ddk95/Inc32/MMDEVLDR.INC
+#file=~/Downloads/win98ddk/98ddk/inc/win98/MMDEVLDR.INC
+file=~/Downloads/winmeddk/NTDDK/inc/win_me/mmdevldr.inc
 count=0
 
 ver="4.90+"
-device=0x0010
+device=0x044A
 
-grep -R "IOS_Service" $file | sed "s/^[\t ]*//" | sed "s/[\t ]\+/"$'\t'"/g" | cut -d $'\t' -f 2 | cut -d ',' -f 1 | sed -e 's/\x0D//g' | sed -e "s/IOS_Service *//" | while read X; do
+grep -R "MMDEVLDR_Service" $file | sed "s/^[\t ]*//" | sed "s/[\t ]\+/"$'\t'"/g" | cut -d $'\t' -f 2 | cut -d ',' -f 1 | sed -e 's/\x0D//g' | sed -e "s/MMDEVLDR_Service *//" | while read X; do
     counth=`printf '%-8s %04XH %04XH  %s' "$ver" $device $count "$X"`;
     echo "$counth"
     count=$(($count+1))
