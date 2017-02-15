f6b876236aef233d3a571b3c93f512c12de0de61
Jonathan Campbell <jonathan@castus.tv>
2017-02-14 17:24:50 -0800

    PCCARD services

--- ddkincservices2str.sh
+++ ddkincservices2str.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 #
-# Example script to take VRDSVC.INC and dump all VCache_Service enumerations
+# Example script to take VRDSVC.INC and dump all PCCARD_Service enumerations
 #file=/mnt/main/emu/win31ddk/DDK/multimed/inc/vsbd.inc
 #file=/mnt/main/emu/win31ddk/DDK/386/include/vrdsvc.inc
-#file=~/Downloads/x/ddk95/Inc32/VCACHE.INC
-#file=~/Downloads/win98ddk/98ddk/inc/win98/VCACHE.INC
-file=~/Downloads/winmeddk/NTDDK/inc/win_me/vcache.inc
+#file=~/Downloads/x/ddk95/Inc32/PCCARD.INC
+#file=~/Downloads/win98ddk/98ddk/inc/win98/PCCARD.INC
+file=~/Downloads/winmeddk/NTDDK/inc/win_me/pccard.inc
 count=0
 
 ver="4.90+"
-device=0x048B
+device=0x097C
 
-grep -R "VCache_Service" $file | sed "s/^[\t ]*//" | sed "s/[\t ]\+/"$'\t'"/g" | cut -d $'\t' -f 2 | cut -d ',' -f 1 | sed -e 's/\x0D//g' | sed -e "s/VCache_Service *//" | while read X; do
+grep -R "PCCARD_Service" $file | sed "s/^[\t ]*//" | sed "s/[\t ]\+/"$'\t'"/g" | cut -d $'\t' -f 2 | cut -d ',' -f 1 | sed -e 's/\x0D//g' | sed -e "s/PCCARD_Service *//" | while read X; do
     counth=`printf '%-8s %04XH %04XH  %s' "$ver" $device $count "$X"`;
     echo "$counth"
     count=$(($count+1))
