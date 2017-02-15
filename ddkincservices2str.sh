3c11c15f1bb8fe90c8dcf6238dfb99ff1e5768f5
Jonathan Campbell <jonathan@castus.tv>
2017-02-14 16:39:18 -0800

    VRedir

--- ddkincservices2str.sh
+++ ddkincservices2str.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 #
-# Example script to take VADLIBD.INC and dump all VADLIBD_Service enumerations
-#file=/mnt/main/emu/win31ddk/DDK/multimed/inc/vadlibd.inc
-#file=/mnt/main/emu/win31ddk/DDK/386/include/vadlibd.inc
-file=~/Downloads/x/ddk95/Inc32/VADLIBD.INC
-#file=~/Downloads/win98ddk/98ddk/inc/win98/VADLIBD.INC
-#file=~/Downloads/winmeddk/NTDDK/inc/win_me/vadlibd.inc
+# Example script to take VRDSVC.INC and dump all VRedir_Service enumerations
+#file=/mnt/main/emu/win31ddk/DDK/multimed/inc/vrdsvc.inc
+#file=/mnt/main/emu/win31ddk/DDK/386/include/vrdsvc.inc
+#file=~/Downloads/x/ddk95/Inc32/VRDSVC.INC
+#file=~/Downloads/win98ddk/98ddk/inc/win98/VRDSVC.INC
+file=~/Downloads/winmeddk/NTDDK/inc/win_me/vrdsvc.inc
 count=0
 
-ver="4.0+"
-device=0x0446
+ver="4.90+"
+device=0x0481
 
-grep -R "VADLIBD_Service" $file | sed "s/^[\t ]*//" | sed "s/[\t ]\+/"$'\t'"/g" | cut -d $'\t' -f 2 | cut -d ',' -f 1 | sed -e 's/\x0D//g' | sed -e "s/VADLIBD_Service *//" | while read X; do
+grep -R "VRedir_Service" $file | sed "s/^[\t ]*//" | sed "s/[\t ]\+/"$'\t'"/g" | cut -d $'\t' -f 2 | cut -d ',' -f 1 | sed -e 's/\x0D//g' | sed -e "s/VRedir_Service *//" | while read X; do
     counth=`printf '%-8s %04XH %04XH  %s' "$ver" $device $count "$X"`;
     echo "$counth"
     count=$(($count+1))
