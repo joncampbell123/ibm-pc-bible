5d5c1a6a7316bd7c56e9a613fc269b57270fad17
Jonathan Campbell <jonathan@castus.tv>
2017-02-14 17:16:20 -0800

    VCache

--- ddkincservices2str.sh
+++ ddkincservices2str.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 #
-# Example script to take VRDSVC.INC and dump all VSBD_Service enumerations
-file=/mnt/main/emu/win31ddk/DDK/multimed/inc/vsbd.inc
+# Example script to take VRDSVC.INC and dump all VCache_Service enumerations
+#file=/mnt/main/emu/win31ddk/DDK/multimed/inc/vsbd.inc
 #file=/mnt/main/emu/win31ddk/DDK/386/include/vrdsvc.inc
-#file=~/Downloads/x/ddk95/Inc32/VRDSVC.INC
-#file=~/Downloads/win98ddk/98ddk/inc/win98/VRDSVC.INC
-#file=~/Downloads/winmeddk/NTDDK/inc/win_me/vrdsvc.inc
+#file=~/Downloads/x/ddk95/Inc32/VCACHE.INC
+#file=~/Downloads/win98ddk/98ddk/inc/win98/VCACHE.INC
+file=~/Downloads/winmeddk/NTDDK/inc/win_me/vcache.inc
 count=0
 
-ver="3.0+"
-device=0x0445
+ver="4.90+"
+device=0x048B
 
-grep -R "VSBD_Service" $file | sed "s/^[\t ]*//" | sed "s/[\t ]\+/"$'\t'"/g" | cut -d $'\t' -f 2 | cut -d ',' -f 1 | sed -e 's/\x0D//g' | sed -e "s/VSBD_Service *//" | while read X; do
+grep -R "VCache_Service" $file | sed "s/^[\t ]*//" | sed "s/[\t ]\+/"$'\t'"/g" | cut -d $'\t' -f 2 | cut -d ',' -f 1 | sed -e 's/\x0D//g' | sed -e "s/VCache_Service *//" | while read X; do
     counth=`printf '%-8s %04XH %04XH  %s' "$ver" $device $count "$X"`;
     echo "$counth"
     count=$(($count+1))
