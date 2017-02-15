73ed75d04cbb03ee0618210d19e0dfd9b0c50f7c
Jonathan Campbell <jonathan@castus.tv>
2017-02-14 17:42:48 -0800

    MTRR services

--- ddkincservices2str.sh
+++ ddkincservices2str.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 #
-# Example script to take VRDSVC.INC and dump all PCI_Service enumerations
+# Example script to take VRDSVC.INC and dump all MTRR_Service enumerations
 #file=/mnt/main/emu/win31ddk/DDK/multimed/inc/vsbd.inc
 #file=/mnt/main/emu/win31ddk/DDK/386/include/vrdsvc.inc
 #file=~/Downloads/x/ddk95/Inc32/PCCARD.INC
-#file=~/Downloads/win98ddk/98ddk/inc/win98/PCI.INC
-file=~/Downloads/winmeddk/NTDDK/inc/win_me/pci.inc
+#file=~/Downloads/win98ddk/98ddk/inc/win98/MTRR.INC
+file=~/Downloads/winmeddk/NTDDK/inc/win_me/mtrr.inc
 count=0
 
 ver="4.90+"
-device=0x0043
+device=0x004A
 
-grep -R "PCI_Service" $file | sed "s/^[\t ]*//" | sed "s/[\t ]\+/"$'\t'"/g" | cut -d $'\t' -f 2 | cut -d ',' -f 1 | sed -e 's/\x0D//g' | sed -e "s/PCI_Service *//" | while read X; do
+grep -R "MTRR_Service" $file | sed "s/^[\t ]*//" | sed "s/[\t ]\+/"$'\t'"/g" | cut -d $'\t' -f 2 | cut -d ',' -f 1 | sed -e 's/\x0D//g' | sed -e "s/MTRR_Service *//" | while read X; do
     counth=`printf '%-8s %04XH %04XH  %s' "$ver" $device $count "$X"`;
     echo "$counth"
     count=$(($count+1))
