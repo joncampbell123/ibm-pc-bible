7457b816fa5adf843414b8cc91c9e0996ab2dd51
Jonathan Campbell <jonathan@castus.tv>
2017-02-14 16:07:05 -0800

    Oh, hello! Apparently the Win9x/ME DDK headers define VXD device 0010H twice: one for the Windows 3.1 BlockDev, one for the Windows 95 IOS.

--- ddkincservices2str.sh
+++ ddkincservices2str.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 #
-# Example script to take IFSMGR.INC and dump all IFSMgr_Service enumerations
-#file=/mnt/main/emu/win31ddk/DDK/386/include/ifsmgr.inc
-#file=~/Downloads/x/ddk95/Inc32/IFSMGR.INC
-#file=~/Downloads/win98ddk/98ddk/inc/win98/IFSMGR.INC
-file=~/Downloads/winmeddk/NTDDK/inc/win_me/ifsmgr.inc
+# Example script to take IOS.INC and dump all IOS_Service enumerations
+#file=/mnt/main/emu/win31ddk/DDK/386/include/ios.inc
+#file=~/Downloads/x/ddk95/Inc32/IOS.INC
+#file=~/Downloads/win98ddk/98ddk/inc/win98/IOS.INC
+file=~/Downloads/winmeddk/NTDDK/inc/win_me/ios.inc
 count=0
 
 ver="4.90+"
-device=0x0040
+device=0x0010
 
-grep -R "IFSMgr_Service" $file | sed "s/^[\t ]*//" | sed "s/[\t ]\+/"$'\t'"/g" | cut -d $'\t' -f 2 | cut -d ',' -f 1 | sed -e 's/\x0D//g' | sed -e "s/IFSMgr_Service *//" | while read X; do
+grep -R "IOS_Service" $file | sed "s/^[\t ]*//" | sed "s/[\t ]\+/"$'\t'"/g" | cut -d $'\t' -f 2 | cut -d ',' -f 1 | sed -e 's/\x0D//g' | sed -e "s/IOS_Service *//" | while read X; do
     counth=`printf '%-8s %04XH %04XH  %s' "$ver" $device $count "$X"`;
     echo "$counth"
     count=$(($count+1))
